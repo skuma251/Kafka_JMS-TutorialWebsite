028be2d7fe4adbe5358f421124e9072fa083db54
Subham Kumar <skuma251@asu.edu>
2022-11-18 12:50:40 -0700

    Updated SendingMessage.html and RecievingMessage.html

---
+++ JMS_codes/JMS2.0/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+import javax.jms.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ConnectionFactory connectionFactory = null;
+        JMSContext jmsContext = connectionFactory.createContext();
+
+    }
+}
